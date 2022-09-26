03fa03c2ea01e5453285db0c4faad06ea80e3396
AdrianAlonso2002 <114055056+AdrianAlonso2002@users.noreply.github.com>
2022-09-26 10:31:43 +0200

    Update ej5b.php

--- ut1/ej5b.php
+++ ut1/ej5b.php
@@ -1,6 +1,7 @@
 <HTML>
 <HEAD><TITLE>  EJ5B – Tabla multiplicar con TD </TITLE></HEAD>
 <style>
+#Le damos css a la tabla.
 table,tr,td{
 	border:1px solid black;
 	border-collapse:collapse;
@@ -15,6 +16,8 @@ $num="8";
 $res="";
 
 #Con un for, va multiplicando cada $i con el número y lo va mostrando en la pantalla dentro del bucle.
+#Se va creando la tabla con echo.
+	
 echo "<table>";
 for ($i=1;$i<=10;$i++){
 	$res=$num*$i;
