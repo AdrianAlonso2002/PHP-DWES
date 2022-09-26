fb7f147c70a0c883a86a32d94a1997d50e7dd6a8
AdrianAlonso2002 <114055056+AdrianAlonso2002@users.noreply.github.com>
2022-09-26 10:05:21 +0200

    Create ej4b.php

---
+++ ut1/ej4b.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD><TITLE>  EJ4B – Tabla Multiplicar </TITLE></HEAD>
+<BODY>
+<?php
+
+#Ej2
+
+$num="8";
+$res="";
+
+#Con un for, va multiplicando cada $i con el número y lo va mostrando en la pantalla dentro del bucle.
+
+for ($i=1;$i<=10;$i++){
+	$res=$num*$i;
+	echo "$num x $i = $res"."<br>";
+}
+
+?>
+</BODY>
+</HTML>
