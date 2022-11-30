c5ae03f1196b3fabf9a8151df1591966ead2556d
AdrianAlonso2002 <114055056+AdrianAlonso2002@users.noreply.github.com>
2022-11-30 13:29:58 +0100

    Update altaemple.php

--- empleadosnn/altaemple.php
+++ empleadosnn/altaemple.php
@@ -31,7 +31,8 @@
 							<label for="dep">CÓDIGO DE DEPARTAMENTO: </label>
 							<select name="dep">
 							<?php
-								dep();
+							require "empleadosnn_fun.php";
+								mostrardepae();
 							?>
 							</select><br>
 							<button id="env" type="submit">Enviar</button>
@@ -43,7 +44,7 @@
 
 						//Siempre hay que poner este if para que el php entienda de donde vienen las variables
 
-						 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+						if ($_SERVER["REQUEST_METHOD"] == "POST") {
 							 
 						// defino variables
 
@@ -69,88 +70,17 @@
 						$sal = test_input($_POST["sal"]);
 						$fec = test_input($_POST["fec"]);
 						$dep = test_input($_POST["dep"]);
+						
+						$error = errorae($dni,$nomb,$sal,$fec,$dep);
+							 
+						if ($error == true) {
+							echo "Corrige los errores.";
+						}else
 						  
 						//Lamamos a la función indicada.
 						 
-						mysql($dni,$nomb,$sal,$fec,$dep);
+						mysqlae($dni,$nomb,$sal,$fec,$dep);
 						
-						}
-		
-						//Funciones
-	
-						function test_input($data) {
-						  $data = trim($data);
-						  $data = stripslashes($data);
-						  $data = htmlspecialchars($data);
-						  return $data;
-						}
-						
-						function dep(){
-							/*SELECTs - mysql PDO*/
-
-							$servername = "localhost";
-							$username = "root";
-							$password = "rootroot";
-							$dbname = "empleadosnn";
-
-							try {
-								$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-								$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-								$stmt = $conn->prepare("SELECT cod_dep FROM dep");
-								$stmt->execute();
-
-								// set the resulting array to associative
-								$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-								foreach($stmt->fetchAll() as $row) {
-									echo "<option>".$row["cod_dep"]."</option>"."<br>";
-								}
-								}
-							catch(PDOException $e) {
-								echo "Error: " . $e->getMessage();
-							}
-							$conn = null;
-						}
-						
-						function mysql($d,$nomb,$sal,$fec,$dep) {
-							
-							
-							/*Inserción en tabla Prepared Statement- mysql PDO*/
-
-							$servername = "localhost";
-							$username = "root";
-							$password = "rootroot";
-							$dbname = "empleadosnn";
-
-							try {
-								$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-								// set the PDO error mode to exception
-								$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-								// prepare sql and bind parameters
-								$stmt = $conn->prepare("INSERT INTO emple (dni,nombre,salario,fec_nac,cod_dep) VALUES (:dni,:nombre,:salario,:fec_nac,:cod_dep)");
-								$stmt->bindParam(':dni', $dni);
-								$stmt->bindParam(':nombre', $nombre);
-								$stmt->bindParam(':salario', $salario);
-								$stmt->bindParam(':fec_nac', $fec_nac);
-								$stmt->bindParam(':cod_dep', $cod_dep);
-  
-								// insert a row
-								$dni = $d;
-								$nombre = $nomb;
-								$salario = $sal;
-								$fec_nac = $fec;
-								$cod_dep = $dep;
-								$stmt->execute();
-
-								// insert another row
-
-								echo "Creado el empleado satisfactoriamente";
-							}
-							catch(PDOException $e)
-							{
-							echo "Error: " . $e->getMessage();
-							}
-							$conn = null;	
 						}
 						
 						?>
@@ -165,3 +95,4 @@
 		</aside>
 </BODY>
 </HTML>
+
